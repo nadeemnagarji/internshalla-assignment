e4b5c1ba9218b9cd000cbd3d010da91c1eb7d91d
Nadeem <nadeemnagarji@gmail.com>
2024-02-27 04:02:12 +0530

    made  website responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Input from '../Input';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import Card from '../maincards/Card';
+import searchIcon from "../../assets/search.png"
 
 FontAwesomeIcon
 export default function Header() {
@@ -32,7 +32,7 @@ export default function Header() {
             <nav className='flex bg bg-darkgrey
              w-full items-center justify-center gap-12 h-14'>
           <div className=' rounded-md flex items-center justify-center gap-2  bg-white'>
-          <fa-faMagnifyingGlass />
+            <img className=' w-5 ml-2' src={searchIcon}/>
             <Input />
           </div>
           <ul className='flex  '>
